39a09b234ee8081172ed00ab2a8024efe7308fa0
nylander <j.a.a.nylander@gmail.com>
2018-01-25 16:55:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Plot Yang's Gamma
+
+- Version: 08/29/2014 03:42:15 PM
+- By: Johan.Nylander\@{nrm|nbis}.se
+
+**TODO:** Fix `ylim` in the code! 
+
+
+## Description
+
+Generate a png plot of the Gamma distribution with different alpha values
+(similar to Fig. 1 in Yang, Z. TREE, 11:367--372, 1996).
+
+
+## Background
+
+One of the more signiciant contributions to the field of computational
+phylogenetics is arguably the introduction of using a Gamma distribution
+to model rate variation across sites (alignment columns).
+
+This was proposed by Ziheng Yang in the beginning of the 1990's, and
+we have all seen examples of the Gamma distribution when changing
+the parameters of the distribution.
+
+From Hillis et al., 1996, p. 443:
+
+>"The Gamma distribution has two parameters,
+>a shape parameter alpha, and a scale parameter beta.
+>By setting beta to 1/alpha, a distribution with mean 1 is obtained."
+
+Unfortunately, we many times had to watch (or even use ourselves!) the 
+original Fig. 1 from Yang's 1996 paper, scanned and pasted in to some
+MS Power Point presentation...
+
+![Fig.1, Yang 1996, scanned and pasted in a presentation](img/Gamma_distribution_Yang.png?raw=true "Fig.1 Yang 1996")
+
+But then came [R](https://www.r-project.org/), and fortunately, things
+were made simple!
+
+![Plot in R using plotYangGamma()](img/ga.png?raw=true "Plot in R using plotYangGamma")
+
+
+## Usage:
+
+Start [R](https://www.r-project.org/), and then
+
+    > source("plotYangGamma.R")
+    > plotYangGamma()
+
+or perhaps
+
+    > alphas <- c(0.1, 0.5, 1.0)
+    > cols <- c("black", "blue", "red")
+    > plotYangGamma(alphas, lwd = 6, col = cols)
+
+or why not
+
+    > plotYangGamma(lwd = 4, col = cols,
+                    xlim = c(0, 3.0), ylim = c(0, 2.0),
+                    file = "Yangs_Gamma.png")
+
+## References:
+
+- Hillis, D., et al.. 1996. Molecular Systematics, 2nd Ed. Sinauer Associates.
+- Yang, Z. 1996. Among-site rate variation and its impact on phylogenetic analyses. TREE, 11:367-372.
+
---
+++ plotYangGamma.R
@@ -0,0 +1,94 @@
+"plotYangGamma" <- function(x, file = NULL, col = rainbow(length(x)), ...) {
+
+    # TODO: solve the ylim!
+
+    # Generate a png plot of the Gamma distribution with different alpha values.
+    # Similar to Fig. 1 in Yang, Z. TREE, 11:367--372, 1996.
+    # 08/29/2014 03:42:15 PM
+    # johan.nylander\@{nrm|nbis}.se
+
+    # From Hillis et al., 1996. Mol. Systematics, p. 443:
+    # "The Gamma distribution has two parameters,
+    # a shape parameter alpha, and a scale parameter beta.
+    # By setting beta to 1/alpha, a distribution with mean 1 is obtained."
+    
+    if(missing(x)) {
+        alphas <- c(0.1, 0.5, 1.0, 5.0, 20.0)
+    } else {
+        alphas <- x
+    }
+
+    cols <- col
+
+    s <- seq(0, 2, 0.01)
+
+    # find ylim
+    arguments <- list(...)
+    if ("ylim" %in% (names(arguments))) {
+        myYlim <- FALSE
+        Ylim <- as.vector(arguments["ylim"])
+    } else {
+        myYlim <- TRUE
+    }
+
+    if(myYlim) {
+        m <- vector()
+        for (i in 1:length(alphas)) {
+            a <- alphas[i]
+            b <- 1/a
+            asort <- sort(dgamma(s, shape = a, scale = b), decreasing = TRUE)
+            if (asort[1] == Inf) {
+                amax <- asort[2]
+            }
+            else {
+                amax <- asort[1]
+            }
+            m[i] <- amax
+        }
+        Ylim <- c(0, max(m))
+    }
+
+    if (!is.null(file)) {
+        png(file = file, width = 600, height = 400)
+    }
+
+    # plot the first value
+    a <- alphas[1]
+    b <- 1/a
+    plot(s, dgamma(s, shape = a, scale = b),
+         type = "l",
+         col = cols[1],
+         ylim = Ylim,
+         xlab = "Substitution rate (r)",
+         yaxt = "no",
+         ylab = "Proportion of sites f(r)",
+         cex.lab = 1.2,
+         cex.main = 2,
+         main = expression(paste(Gamma, "(", alpha, ",", beta, "=1/", alpha, ")", " distribution")), ...)
+
+    # add the rest
+    if ( is.na(alphas[2]) ) {
+        if (!is.null(file)) {
+            legend(x = "topright", legend = alphas, col = cols, lty = 1, bty = "n", ...)
+            dev.off() 
+        }
+        else {
+            legend(x = "topright", legend = alphas, col = cols, lty = 1, bty = "n", ...)
+        }
+    }
+    else {
+        for (i in 2:length(alphas)) {
+            a <- alphas[i]
+            b <- 1/a;
+            lines(s, dgamma(s, shape = a, scale = b), type = "l", col = cols[i], ylim = Ylim, ...)
+        }
+        if (!is.null(file)) {
+            legend(x = "topright", legend = alphas, col = cols, lty = 1, bty = "n", ...)
+            dev.off() 
+        }
+        else {
+            legend(x = "topright", legend = alphas, col = cols, lty = 1, bty = "n", ...)
+        }
+    }
+}
+
